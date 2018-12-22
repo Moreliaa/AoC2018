ae400c36de99ad916be6a85bc7f2356b1b87b90a
Moreliaa <36938007+Moreliaa@users.noreply.github.com>
2018-12-22 22:56:01 +0100

    Day22

--- src/days/Day22.java
+++ src/days/Day22.java
@@ -1,16 +1,29 @@
 package days;
 
 import java.util.*;
+import java.util.Map.*;
+import java.util.regex.*;
 
 public class Day22 {
+
 	private static int depth = 11541;
 	private static int xTarget = 14;
 	private static int yTarget = 778;
+	// private static int depth = 510;
+	// private static int xTarget = 10;
+	// private static int yTarget = 10;
+	private static int maxOffset = 300;
 	private static HashMap<String, Integer> indexMap = new HashMap<String, Integer>(10000);
 	private static HashMap<String, Integer> erosionMap = new HashMap<String, Integer>(10000);
 	private static HashMap<String, Integer> typeMap = new HashMap<String, Integer>(10000);
 
+	// dijkstra
+	private static HashMap<String, Integer> visited = new HashMap<String, Integer>(10000);
+	private static HashMap<String, Integer> distanceMap = new HashMap<String, Integer>(10000);
+	private static HashMap<String, String> toolMap = new HashMap<String, String>(10000);
+
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
 		// Part1
 		for (int y = 0; y <= yTarget; y++)
 			for (int x = 0; x <= xTarget; x++)
@@ -21,9 +34,108 @@ public class Day22 {
 				sum += typeMap.get(x + "," + y);
 		System.out.println(sum);
 		// Part2
-		for (int y = 0; y <= depth; y++)
-			for (int x = 0; x <= depth; x++)
+		for (int y = yTarget; y <= yTarget + maxOffset; y++)
+			for (int x = xTarget; x <= xTarget + maxOffset; x++)
 				setErosion(x, y);
+		// printMap();
+		dijkstra();
+		long end = System.currentTimeMillis();
+		long runningtime = end - start;
+		System.out.println("Running time: " + runningtime + "ms");
+	}
+
+	private static void dijkstra() {
+
+		int currX = 0;
+		int currY = 0;
+		String currentTool = "torch";
+		int currentDistance = 0;
+		// tools: neither, both, torch, gear, notTorch, notGear
+		Pattern p = Pattern.compile("(\\d+),(\\d+)");
+
+		while (true) {
+			// calc distances
+			calcTentativeDistance(currX, currY - 1, currentTool, currentDistance);
+			calcTentativeDistance(currX - 1, currY, currentTool, currentDistance);
+			calcTentativeDistance(currX + 1, currY, currentTool, currentDistance);
+			calcTentativeDistance(currX, currY + 1, currentTool, currentDistance);
+			visited.put(currX + "," + currY, 0);
+			if (visited.containsKey(xTarget + "," + yTarget))
+				break;
+			distanceMap.remove(currX + "," + currY);
+			// find next node to visit
+			Iterator<Entry<String, Integer>> it = distanceMap.entrySet().iterator();
+			int shortest = 99999999;
+			String shortestKey = "";
+			while (it.hasNext()) {
+				Map.Entry<String, Integer> pair = it.next();
+				String key = pair.getKey();
+				if (!visited.containsKey(key)) {
+					int dist = pair.getValue();
+					if (dist < shortest) {
+						shortestKey = key;
+						shortest = dist;
+					}
+				}
+			}
+			Matcher m = p.matcher(shortestKey);
+			m.find();
+			currX = Integer.valueOf(m.group(1));
+			currY = Integer.valueOf(m.group(2));
+			currentTool = toolMap.get(shortestKey);
+			currentDistance = shortest;
+		}
+		System.out.println(currentDistance);
+
+	}
+
+	private static void calcTentativeDistance(int x, int y, String currentTool, int totalDistance) {
+		if (x >= 0 && x <= xTarget + maxOffset && y >= 0 && y <= yTarget + maxOffset
+				&& !visited.containsKey(x + "," + y)) {
+			int type = typeMap.get(x + "," + y);
+			int distance = 1 + totalDistance;
+			String newTool = currentTool;
+			switch (type) {
+			case 0: // rocky
+				if (currentTool == "neither"
+						|| (x == xTarget && y == yTarget && (currentTool == "notTorch" || currentTool == "gear"))) {
+					newTool = "both";
+					distance += 7;
+				}
+				break;
+			case 1:// wet
+				if (currentTool == "torch") {
+					newTool = "notTorch";
+					distance += 7;
+				} else if (currentTool == "both") {
+					newTool = "gear";
+				} else if (currentTool == "notGear") {
+					newTool = "neither";
+				}
+				break;
+			case 2:// narrow
+				if (currentTool == "gear") {
+					newTool = "notGear";
+					distance += 7;
+				} else if (currentTool == "both") {
+					newTool = "torch";
+				} else if (currentTool == "notTorch") {
+					newTool = "neither";
+				}
+			}
+			if (!distanceMap.containsKey(x + "," + y) || distance < distanceMap.get(x + "," + y)) {
+				distanceMap.put(x + "," + y, distance);
+				toolMap.put(x + "," + y, newTool);
+			}
+		}
+	}
+
+	private static void printMap() {
+		for (int y = 0; y < yTarget + maxOffset; y++) {
+			for (int x = 0; x < xTarget + maxOffset; x++)
+				System.out.print(typeMap.get(x + "," + y));
+			System.out.println();
+		}
 	}
 
 	private static int getIndex(int x, int y) {
